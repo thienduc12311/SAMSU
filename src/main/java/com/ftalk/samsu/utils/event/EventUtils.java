c7cb9898087ba8a082f7c14618697c6f42ebdabe
Duc Nguyen <ducn@intelisysaviation.com>
2023-11-14 07:02:12 +0700

    update s3 url

--- src/main/java/com/ftalk/samsu/utils/event/EventUtils.java
+++ src/main/java/com/ftalk/samsu/utils/event/EventUtils.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class EventUtils {
-    private static final String DOMAIN_S3 = "https://samsu.sgp1.cdn.digitaloceanspaces.com/";
+    private static final String DOMAIN_S3 = "https://sgp1.digitaloceanspaces.com/samsu/";
     private static final String SPLIT_KEYWORD = "$$$";
     public static boolean validateFileUrlsS3(String fileUrls){
         return !Arrays.stream(fileUrls.split(SPLIT_KEYWORD)).anyMatch((s) -> !validateUrlS3(s));
