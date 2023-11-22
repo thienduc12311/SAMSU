d175fe066abcee9e448c3cc2dee9ae277ed70302
huytna <huytna@vng.com.vn>
2023-11-22 08:59:39 +0700

    Update: update endpoint grade policy

---
+++ src/main/java/com/ftalk/samsu/payload/event/TaskResponse.java
@@ -0,0 +1,47 @@
+package com.ftalk.samsu.payload.event;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ftalk.samsu.model.event.Event;
+import com.ftalk.samsu.model.event.Task;
+import com.ftalk.samsu.model.feedback.FeedbackQuestion;
+import com.ftalk.samsu.model.gradePolicy.GradeSubCriteria;
+import com.ftalk.samsu.model.semester.Semester;
+import com.ftalk.samsu.model.user.Department;
+import com.ftalk.samsu.model.user.User;
+import com.ftalk.samsu.payload.gradePolicy.GradeSubCriteriaResponse;
+import com.ftalk.samsu.payload.user.UserProfileReduce;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class TaskResponse {
+    private Integer id;
+    private String title;
+    private String content;
+    private Short status;
+    private UserProfileReduce creator;
+    private Integer eventId;
+    private Short score;
+    private GradeSubCriteriaResponse gradeSubCriteria;
+
+    public TaskResponse(Task task) {
+        id = task.getId();
+        title = task.getTitle();
+        content = task.getContent();
+        status = task.getStatus();
+        creator = task.getCreatorUserId() != null ? new UserProfileReduce(task.getCreatorUserId()) : null;
+        eventId = task.getEvent() != null ? task.getEvent().getId() : null;
+        score = task.getScore();
+        gradeSubCriteria = new GradeSubCriteriaResponse(task.getGradeSubCriteria());
+    }
+
+
+}
